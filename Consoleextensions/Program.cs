2b29bb96100b406e281aba1f80dcc155c715487e
Valentina Tamayo <tamayovalentina97@gmail.com>
2026-02-18 19:55:35 -0500

    Shared

---
+++ Consoleextensions/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Shared;
+
+var answer = string.Empty;
+
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var number = ConsoleExtenison.GetInt("Ingrese numero entero diferente de cero: ");
+    if (number == 0)
+    {
+        continue;
+
+    }
+    if (number % 2 == 0)
+
+    {
+        Console.WriteLine($"El numero {number}, es par");
+
+    }
+    else
+    {
+        Console.WriteLine($"El numero {number}, es impar.");
+    }
+    do
+    {
+        answer = ConsoleExtenison.GetValidOptions("¿Deseas continuar [S]i, [N]o?: ", options);
+
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+}  while(answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game over.");
---
+++ Shared/ConsoleExtension.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Shared
+{
+    public class ConsoleExtenison
+    {
+        public static int GetInt(string message)
+        {
+            Console.Write(message);
+            var numberString = Console.ReadLine();
+            if (int.TryParse(numberString, out int numberint))
+            {
+                return numberint;
+            }
+            return 0;
+        }
+
+
+
+        public static string? GetValidOptions(string message, List<string> options)//public static string? la ? es para validar nulidad
+        {
+            Console.Write(message);
+            var answer = Console.ReadLine();
+            if (options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)))// funcion any para validar si la respuesta del usuario es igual a alguna de las opciones que tenemos en la lista, el string comparison es para validar que no importe si el usuario escribe mayuscula o minuscula
+            {
+                return answer;
+
+            }
+
+            return null;
+
+        }
+    }
+}
+
